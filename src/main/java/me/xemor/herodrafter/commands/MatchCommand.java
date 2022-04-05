51021003182f9b03445d7331f1054140c94148bb
Xemorr <31805746+Xemorr@users.noreply.github.com>
2022-04-05 21:35:30 +0100

    Added Abandon Button

--- src/main/java/me/xemor/herodrafter/commands/MatchCommand.java
+++ src/main/java/me/xemor/herodrafter/commands/MatchCommand.java
@@ -1,6 +1,7 @@
 package me.xemor.herodrafter.commands;
 
 import me.xemor.herodrafter.HeroDrafter;
+import me.xemor.herodrafter.Player;
 import me.xemor.herodrafter.match.Match;
 import me.xemor.herodrafter.match.MatchException;
 import me.xemor.herodrafter.match.MatchHandler;
@@ -10,8 +11,6 @@ import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.interactions.components.Button;
-
-import java.util.concurrent.CompletableFuture;
 
 public class MatchCommand implements Command, ButtonHandler {
 
@@ -35,10 +34,13 @@ public class MatchCommand implements Command, ButtonHandler {
         long matchSize = e.getOption("match-size").getAsLong();
         try {
             Match match = HeroDrafter.getMatchHandler().draftMatch(matchSize);
+            HeroDrafter.getMatchHandler().getQueue().removeAll(match.getTeam1().getPlayersToHero().keySet());
+            HeroDrafter.getMatchHandler().getQueue().removeAll(match.getTeam2().getPlayersToHero().keySet());
             e.getHook().sendMessageEmbeds(match.generateGameEmbed(), match.generateTeamEmbed(match.getTeam1(), "A"), match.generateTeamEmbed(match.getTeam2(), "B"))
                     .addActionRow(Button.primary("TeamARoundWin" + match.getTimestamp(), "Team A Round Win").withEmoji(Emoji.fromUnicode("\uD83C\uDD70"))
                             ,Button.primary("TeamBRoundWin" + match.getTimestamp(), "Team B Round Win").withEmoji(Emoji.fromUnicode("\uD83C\uDD71")),
-                            Button.danger("MatchEnd" + match.getTimestamp(), "End the Match").withEmoji(Emoji.fromUnicode("\uD83D\uDC40"))).queue();
+                            Button.danger("MatchEnd" + match.getTimestamp(), "End the Match").withEmoji(Emoji.fromUnicode("\uD83D\uDC40")),
+                            Button.danger("Abandon" + match.getTimestamp(), "Abandon").withEmoji(Emoji.fromUnicode("\uD83D\uDEA8"))).queue();
             HeroDrafter.getMatchHandler().playMatch(match);
         } catch (MatchException ex) {
             e.getHook().sendMessage(ex.getMessage()).queue();
@@ -56,6 +58,7 @@ public class MatchCommand implements Command, ButtonHandler {
             case "TeamARoundWin" -> roundWin(e, match, true);
             case "TeamBRoundWin" -> roundWin(e, match, false);
             case "MatchEnd" -> matchEnd(e, match);
+            case "Abandon" -> matchAbandon(e, match);
         }
     }
 
@@ -74,5 +77,9 @@ public class MatchCommand implements Command, ButtonHandler {
         HeroDrafter.getMatchHandler().endMatch(match);
     }
 
+    public void matchAbandon(ButtonClickEvent e, Match match) {
+        e.getMessage().delete().queue();
+        HeroDrafter.getMatchHandler().endMatch(match);
+    }
 
 }
